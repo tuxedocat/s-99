f23ead17b192b4e39f5f0d38320538e5c726e74d
tuxedocat <yusawai.null@gmail.com>
2016-09-06 22:47:01 +0900

    Refactored lists

--- src/main/scala/s99/P05.scala
+++ src/main/scala/s99/P05.scala
@@ -5,4 +5,7 @@ object P05 {
     case x :: xs => reverse(xs) ::: List(x)
     case _ => Nil
   }
+
+  // foldLeft version
+  //  def reverse[T](list: List[T]): List[T] = list.foldLeft(Nil: List[T])((x, y) => y :: x)
 }
